d40fbb3b5419dad9c8c6bddaa74533c1487435a3
ben.chalmers <ben.chalmers@learnplatform.com>
2021-10-27 21:32:30 -0400

    iterates through .tiff files and extracts data

--- extract.py
+++ extract.py
@@ -1,15 +1,20 @@
 # from exif import Image
 import PIL
 import exifread
+import os
 
 
-img_path = '/Users/benchalmers/Documents/photomap/pics/IMG_9194.TIFF'
+# img_path = '/Users/benchalmers/Documents/photomap/pics/IMG_9194.TIFF'
+directory = '/Users/benchalmers/Documents/photomap/pics/'
 
+
+# Converts GPS coords from deg/min/sec to decimal
 def decimal_coords(coords, ref):
     decimal_degrees = float(coords[0]) + float(coords[1])/60 + convert_to_float(coords[2])/3600
     if ref == 'S' or ref == 'W':
         decimal_degrees *= -1
     return decimal_degrees
+
 
 # Custom float conversion since fractions in GPS data causing bugs
 def convert_to_float(frac_str):
@@ -25,54 +30,62 @@ def convert_to_float(frac_str):
         frac = float(num) / float(denom)
         return whole - frac if whole < 0 else whole + frac
 
-with open(img_path, 'rb') as src:
-    # img = Image(src)
-    print ("Source Image:", src.name)
-    tags = exifread.process_file(src)
 
-    # These tell us N/S and E/W to determine whether decimal
-    # Needs to be negative or not. 
-    gps_longref = str(tags['GPS GPSLongitudeRef'])
-    gps_latref = str(tags['GPS GPSLatitudeRef'])
-    
-    # Decimal latitude
-    gps_lat_temp = str(tags['GPS GPSLatitude'])
-    gps_lat_temp = gps_lat_temp.replace("[","")
-    gps_lat_temp = gps_lat_temp.replace("]","")
-    gps_lat_temp = gps_lat_temp.split(",")
-    lat = decimal_coords(gps_lat_temp, gps_latref)
-    
-    #Decimal longitude
-    gps_long_temp = str(tags['GPS GPSLongitude'])
-    gps_long_temp = gps_long_temp.replace("[","")
-    gps_long_temp = gps_long_temp.replace("]","")
-    gps_long_temp = gps_long_temp.split(",")
-    long = decimal_coords(gps_long_temp, gps_longref)
-
-    # Elevation (meters)
-    ele = convert_to_float(str(tags['GPS GPSAltitude']))
-
-    # Date Taken
-    date = str(tags['EXIF DateTimeOriginal'])
-
-    def call_vars():
-        vars_tuple = (lat, long, ele, date)
-        return(vars_tuple)
+# loop thru directory extracting relevant data for files,
+# output dict of image: tuple
+def files_puller():
+    data_dict = {}
+    for filename in os.listdir(directory):
+        if filename.endswith('.tiff'): # or filename.endswith('.mov')
+            img_path = os.path.join(directory, filename)
+            data_dict[filename] = pulling_vars(img_path)  
+        else: 
+            continue
+    return(data_dict)
 
 
-print(call_vars())
+def pulling_vars(img_path):
+    with open(img_path, 'rb') as src:
+        # print ("Source Image:", src.name)
+        tags = exifread.process_file(src)
+
+        # These tell us N/S and E/W to determine whether decimal
+        # Needs to be negative or not (arg in decimal_coords). 
+        gps_longref = str(tags['GPS GPSLongitudeRef'])
+        gps_latref = str(tags['GPS GPSLatitudeRef'])
+        
+        # Decimal latitude
+        gps_lat_temp = str(tags['GPS GPSLatitude'])
+        gps_lat_temp = gps_lat_temp.replace("[","")
+        gps_lat_temp = gps_lat_temp.replace("]","")
+        gps_lat_temp = gps_lat_temp.split(",")
+        lat = decimal_coords(gps_lat_temp, gps_latref)
+        
+        #Decimal longitude
+        gps_long_temp = str(tags['GPS GPSLongitude'])
+        gps_long_temp = gps_long_temp.replace("[","")
+        gps_long_temp = gps_long_temp.replace("]","")
+        gps_long_temp = gps_long_temp.split(",")
+        long = decimal_coords(gps_long_temp, gps_longref)
+
+        # Elevation (meters)
+        ele = convert_to_float(str(tags['GPS GPSAltitude']))
+
+        # Date Taken
+        date = str(tags['EXIF DateTimeOriginal'])
+        vars_dict = {'lat':lat, 'long':long, 'ele':ele, 'date':date}
+        # print(img_path)
+        # print(vars_dict)
+        return(vars_dict)
 
 
-
-
+print(files_puller())
 
 
 
 
 # Next steps: 
-# 1. extract gps coords and convert format 
-# 2. extract altitude also
-# 2b. and 'EXIF DateTimeOriginal'
+
 # 3. make it so it loops through file list and gets it from all 
 #     images/files if they have exif
 # 4. output from this file should be dictionary: image filename as key, tuple of GPS and alt as value 
